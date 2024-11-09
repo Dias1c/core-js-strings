0d844346c095d4e668f9c65a23d71eb803ab4a63
Dias1c <dias.kappasov@gmail.com>
2024-11-09 22:31:29 +0500

    feat: realize invertCase

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -362,8 +362,19 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((c) => {
+      if (c >= 'a' && c <= 'z') {
+        return c.toUpperCase();
+      }
+      if (c >= 'A' && c <= 'Z') {
+        return c.toLowerCase();
+      }
+      return c;
+    })
+    .join('');
 }
 
 /**
